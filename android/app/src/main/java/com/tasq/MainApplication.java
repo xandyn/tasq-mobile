1ce771321af3a769c7781c0d1d3fa8c6e1035095
xandyn <mail.vagapov@gmail.com>
2017-04-14 09:17:29 +0500

    Updated android app
    • installed `react-native-navigation` package
    • installed `react-native-vector-icons` package

--- android/app/src/main/java/com/tasq/MainApplication.java
+++ android/app/src/main/java/com/tasq/MainApplication.java
@@ -1,35 +1,30 @@
 package com.tasq;
 
-import android.app.Application;
+import android.support.annotation.NonNull;
 
-import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactnativenavigation.NavigationApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
-    }
-  };
+public class MainApplication extends NavigationApplication {
 
   @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+  public boolean isDebug() {
+    return BuildConfig.DEBUG;
+  }
+
+  @NonNull
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+    // Add the packages you require here.
+    // No need to add RnnPackage and MainReactPackage
+    return Arrays.<ReactPackage>asList(
+            new VectorIconsPackage()
+    );
   }
 
   @Override
